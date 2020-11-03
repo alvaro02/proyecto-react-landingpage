db8b866cbfc3c9e0d2f5bf4312a83272aa37ed46
Alvaro G <alvaro.guajardo02@inacapmail.cl>
2020-11-02 21:50:40 -0300

    Final

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Slider  from './components/Slider';
 import { Nav } from './components/Nav';
 import { Card } from './components/Card';
+import { Form } from './components/Form';
+import { Footer } from './components/Footer';
 
 const App = () => {
     return (
@@ -19,6 +21,9 @@ const App = () => {
                     <Card photo='https://picsum.photos/800/600' />
                 </div>
             </div>
+            <div className=" text-4xl w-full mb-10 flex justify-center">Formulario</div>
+            <Form />
+            <Footer />
         </div>
     )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <div className="mt-5 flex justify-center bg-blue-700">
+            <p className="text-white">Alvaro Guajardo  2020/P Programación Interactiva (INPI02/32/D Apoquindo DWM)</p>        
+        </div>
+    )
+}
