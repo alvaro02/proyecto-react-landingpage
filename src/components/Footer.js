f5f32c90ab17d16a7e2a8627e56d63bfe981dc87
Alvaro G <alvaro.guajardo02@inacapmail.cl>
2020-11-02 22:49:22 -0300

    Final

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Footer = () => {
     return (
-        <div className="mt-5 flex justify-center bg-blue-700">
+        <div className="w-screen mt-5 flex justify-center bg-blue-700">
             <p className="text-white">Alvaro Guajardo  2020/P Programación Interactiva (INPI02/32/D Apoquindo DWM)</p>        
         </div>
     )
